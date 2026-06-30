8a11964362b8cb4b3ce5da06cf16f815d2397537
Vishal Choudhary <vishalchoudhary.mail@gmail.com>
2026-06-30 21:02:30 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const closingBracket = {
+            ')' : '(',
+            '}' : '{',
+            ']' :'['
+        };
+
+        const stack = [];
+        for(let bracket of s){
+            if(closingBracket[bracket]){
+                if(!stack.length || closingBracket[bracket] !== stack.pop()) return false;
+            }
+            else{
+                stack.push(bracket);
+            }
+        }   
+        return !stack.length;
+    }
+}
