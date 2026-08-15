456a1889fb5d174a98d51e6e3d40e7b6e02342fb
Vishal Choudhary <vishalchoudhary.mail@gmail.com>
2026-08-15 23:16:53 +0530

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+        let ans = new Array(word1.length + word2.length);
+        let i = 0;
+        while( i< word1.length && i< word2.length){
+            ans.push(word1[i], word2[i]);
+            i++;
+        }
+        while( i< word1.length){
+            ans.push(word1[i]);
+            i++;
+        }
+        while( i< word2.length){
+            ans.push(word2[i]);
+            i++;
+        }
+
+        return ans.join("");
+    }
+}
