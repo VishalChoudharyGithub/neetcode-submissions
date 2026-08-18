7f645565f9590cb0e41d5c7c1463fb4635568e88
Vishal Choudhary <vishalchoudhary.mail@gmail.com>
2026-08-18 23:18:07 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} weights
+     * @param {number} days
+     * @return {number}
+     */
+    shipWithinDays(weights, days) {
+        let l = Math.max(...weights);
+        let r = weights.reduce((a, b) => a + b, 0);
+        let res = r;
+
+        function canShip(cap) {
+            let ships = 1,
+                currCap = cap;
+            for (const w of weights) {
+                if (currCap - w < 0) {
+                    ships++;
+                    if (ships > days) {
+                        return false;
+                    }
+                    currCap = cap;
+                }
+                currCap -= w;
+            }
+            return true;
+        }
+
+        while (l <= r) {
+            const cap = l + Math.floor((r - l) / 2);
+
+            if (canShip(cap)) {
+                res = Math.min(res, cap);
+                r = cap - 1;
+            } else {
+                l = cap + 1;
+            }
+        }
+
+        return res;
+    }
+}
